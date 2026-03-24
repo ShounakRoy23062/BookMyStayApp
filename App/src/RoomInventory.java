6b43e73baca6cdef347c6e1084c00d95e1ea35b5
ShounakRoy23062 <shounakofficial02@gmail.com>
2026-03-24 10:01:56 +0530

    Use Case 12: Data Persistence & System Recovery

--- App/src/RoomInventory.java
+++ App/src/RoomInventory.java
@@ -11,15 +11,20 @@ public class RoomInventory {
         rooms.put("Suite", 2);
     }
 
+    public void setRoom(String roomType, int count) {
+        rooms.put(roomType, count);
+    }
+
     public int getAvailability(String roomType) {
         return rooms.getOrDefault(roomType, 0);
     }
 
-    public void decrementRoom(String roomType) {
-        rooms.put(roomType, rooms.get(roomType) - 1);
-    }
-
     public Map<String, Integer> getAllRooms() {
         return rooms;
+    }
+
+    public void displayInventory() {
+        System.out.println("Current Inventory:");
+        rooms.forEach((type, count) -> System.out.println(type + ": " + count));
     }
 }
